e9fee033199f103e8c7e05675e0a8b4bfdb5e218
ivan.cheung <ivan.cheung@airbnb.com>
2018-11-17 17:36:35 +0800

    Initial Commit

---
+++ DownloadManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DownloadManager
+//
+//  Created by ivan_man-kit-cheung on 11/17/18.
+//  Copyright © 2018 ivan_man-kit-cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
